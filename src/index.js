df977f09d947f9bb591fcbba48403bf9d6da3f4e
Anderson Zanichelli <anderson.zanichelli@objective.com.br>
2017-07-12 18:53:39 -0300

    Post show

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostShow from './components/posts_show';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -16,8 +17,9 @@ ReactDOM.render(
     <BrowserRouter>
       <div>
           <Switch>
-            <Route path="/posts/new" component={PostsNew} />
-            <Route path="/" component={PostsIndex} />
+            <Route path="/posts/new" component={ PostsNew } />
+            <Route path="/posts/:id" component={ PostShow } />
+            <Route path="/" component={ PostsIndex } />
           </Switch>
       </div>
     </BrowserRouter>
